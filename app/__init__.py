750244683c273d33137bc46ea9b68962276a927a
zhengzongwei <zhengzongwei@foxmail.com>
2021-04-07 14:02:53 +0800

    add log

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from sanic import Sanic
 from config.config import config_map
+from config.log_config import LOG_SETTINGS
 from api import api_v1
 
 
@@ -8,6 +9,7 @@ def create_app(config=None):
 
     app = Sanic(__name__)
     app.config.update_config(config_map[config])
+    app.config.update_config(LOG_SETTINGS)
 
     app.blueprint(api_v1,url_prefix="/")
 
